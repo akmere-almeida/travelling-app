e79b641569f03367b4bbc09e13949d5790d7ce04
Akme-re <akmere.almeida@thoughtworks.com>
2021-10-03 10:50:04 -0300

    chore: add buildSrc for project configuration

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,10 @@
+object Dependencies {
+
+    object Kotlin {
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+    }
+
+    object Android {
+        const val material = "com.google.android.material:material:${Versions.material}"
+    }
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,5 @@
+object Versions {
+    const val gradle = "4.2.1"
+    const val kotlin = "1.5.0"
+    const val material = "1.4.0"
+}
