66966177c677eccbecdc8b788683fc387fa4f978
Akme-re <akmere.almeida@thoughtworks.com>
2021-10-12 12:06:37 -0300

    feat: add GraphqlNetworkClient.kt implementation

--- app/src/main/java/com/akmere/travelling_app/data/networking/GraphqlNetworkClient.kt
+++ app/src/main/java/com/akmere/travelling_app/data/networking/GraphqlNetworkClient.kt
@@ -1,11 +1,15 @@
 package com.akmere.travelling_app.data.networking
 
+import com.akmere.travelling_app.data.DataConstants
 import com.apollographql.apollo.ApolloClient
 import okhttp3.OkHttpClient
 
 class GraphqlNetworkClient(private val okHttpClient: OkHttpClient) {
 
     fun providesClient(): ApolloClient {
-        TODO()
+        return ApolloClient.builder()
+            .serverUrl(DataConstants.serverUrl)
+            .okHttpClient(okHttpClient)
+            .build()
     }
 }
