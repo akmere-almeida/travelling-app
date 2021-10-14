d9f9a1485028486b8dcee23c160b333a2e116bdf
Akme-re <akmere.almeida@thoughtworks.com>
2021-10-14 10:04:48 -0300

    test: make tests pass

--- app/src/main/java/com/akmere/travelling_app/data/repository/PackageOfferRepository.kt
+++ app/src/main/java/com/akmere/travelling_app/data/repository/PackageOfferRepository.kt
@@ -5,10 +5,28 @@ import com.akmere.travelling_app.data.model.AddressData
 import com.akmere.travelling_app.data.model.GalleryData
 import com.akmere.travelling_app.data.model.ImageData
 import com.akmere.travelling_app.data.model.PackageOfferData
+import com.akmere.travelling_app.data.repository.exceptions.OfferParseException
+import com.akmere.travelling_app.data.repository.exceptions.UnexpectedLoadException
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.coroutines.await
-import okio.IOException
+import com.apollographql.apollo.exception.ApolloParseException
+import java.io.IOException
+import java.lang.Exception
+import java.lang.IllegalStateException
 
+/**
+ * Interface usada para comunicação com API de Ofertas de Pacotes
+ *
+ *
+ * Carrega as informações de Ofertas
+ *
+ * @param datasource cliente GraphQL para carregar informações da API
+ *
+ * @return uma lista de [PackageOfferData] contendo informações de oferta dos pacotes
+ *
+ * @throws OfferParseException
+ * @throws UnexpectedLoadException
+ */
 class PackageOfferRepository(private val datasource: ApolloClient) :
     OfferRepository<PackageOfferData> {
 
@@ -16,27 +34,38 @@ class PackageOfferRepository(private val datasource: ApolloClient) :
         imagesPerOffer: Int,
         searchTerms: List<String>
     ): List<PackageOfferData> {
-        val queryResult = datasource.query(SearchPackageQuery(searchTerms, imagesPerOffer)).await()
-        return queryResult.data?.searchPackage?.results?.map {
-            it.toPackageOfferData()
-        } ?: throw IOException()
+        try {
+            val queryResult =
+                datasource.query(SearchPackageQuery(searchTerms, imagesPerOffer)).await()
+            try {
+                return queryResult.data?.searchPackage?.results?.map {
+                    it.toPackageOfferData()
+                } ?: throw OfferParseException()
+            } catch (e: ApolloParseException) {
+                throw OfferParseException()
+            }
+        } catch (e: Exception) {
+            throw UnexpectedLoadException()
+        }
+    }
+
+    private fun SearchPackageQuery.Result.toPackageOfferData(): PackageOfferData {
+        return PackageOfferData(
+            name = name,
+            description = description,
+            isAvailable = isAvailable,
+            addressData = AddressData(address.city, address.country),
+            galleryData = gallery.map { image ->
+                image.toImageData(name)
+            }.run {
+                GalleryData(this)
+            }
+        )
+    }
+
+    private fun SearchPackageQuery.Gallery.toImageData(defaultDescription: String): ImageData {
+        return ImageData(url, description = description ?: defaultDescription)
     }
 }
 
-fun SearchPackageQuery.Result.toPackageOfferData(): PackageOfferData {
-    return PackageOfferData(
-        name = name,
-        description = description,
-        isAvailable = isAvailable,
-        addressData = AddressData(address.city, address.country),
-        galleryData = gallery.map { image ->
-            image.toImageData()
-        }.run {
-            GalleryData(this)
-        }
-    )
-}
 
-fun SearchPackageQuery.Gallery.toImageData(): ImageData {
-    return ImageData(url, description = description)
-}
