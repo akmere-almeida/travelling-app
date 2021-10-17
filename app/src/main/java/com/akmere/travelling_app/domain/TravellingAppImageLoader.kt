b18146252dfb462e294a3bdfb7b07c9400889d09
Akme-re <akmere.almeida@thoughtworks.com>
2021-10-16 23:09:11 -0300

    feat: add image loader encapsulation over coil to load resources into the UI

--- app/src/main/java/com/akmere/travelling_app/domain/TravellingAppImageLoader.kt
+++ app/src/main/java/com/akmere/travelling_app/domain/TravellingAppImageLoader.kt
@@ -1,15 +1,40 @@
 package com.akmere.travelling_app.domain
 
+import android.content.Context
 import android.graphics.Bitmap
-import androidx.compose.ui.graphics.ImageBitmap
+import android.graphics.drawable.Drawable
+import android.util.Log
+import androidx.annotation.VisibleForTesting
+import androidx.core.graphics.drawable.toBitmap
 import coil.ImageLoader
+import coil.request.ImageRequest
+import com.akmere.travelling_app.common.Logger
+import com.akmere.travelling_app.domain.errors.ImageLoadError
 
-class TravellingAppImageLoader(private val imageLoader: ImageLoader) {
-    suspend fun loadFromNetwork(imageUrl: String): ImageBitmap {
-        TODO()
+class TravellingAppImageLoader(
+    private val imageLoader: ImageLoader,
+    private val context: Context,
+    private val logger: Logger? = null
+) {
+    suspend fun loadFromNetwork(imageUrl: String): Drawable {
+        val request = requestFromImageUrl(imageUrl)
+        return try {
+            imageLoader.execute(request).drawable ?: throw ImageLoadError()
+        } catch (e: Exception) {
+            logger?.log(TAG, Log.INFO, message = e.message, throwable = e)
+            throw ImageLoadError(e.message)
+        }
     }
 
-    suspend fun loadFromNetwork(imageUrls: List<String>): List<Bitmap> {
-        TODO()
+    suspend fun loadFromNetwork(imageUrls: List<String>): List<Drawable> {
+        return imageUrls.map { loadFromNetwork(it) }
+    }
+
+    private fun requestFromImageUrl(imageUrl: String): ImageRequest {
+        return ImageRequest.Builder(context).data(imageUrl).build()
+    }
+
+    companion object {
+        private const val TAG = "TravellingAppImageLoader"
     }
 }
--- app/src/test/java/com/akmere/travelling_app/domain/TravellingAppImageLoaderTest.kt
+++ app/src/test/java/com/akmere/travelling_app/domain/TravellingAppImageLoaderTest.kt
@@ -1,15 +1,17 @@
 package com.akmere.travelling_app.domain
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
+import androidx.core.graphics.drawable.toBitmap
 import coil.ImageLoader
 import coil.request.ImageRequest
 import coil.request.ImageResult
-import coil.request.NullRequestDataException
+import com.akmere.travelling_app.common.Logger
 import com.akmere.travelling_app.domain.errors.ImageLoadError
 import io.mockk.*
 import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -17,39 +19,41 @@ class TravellingAppImageLoaderTest {
     @RelaxedMockK
     private lateinit var imageLoader: ImageLoader
 
+    @RelaxedMockK
+    private lateinit var logger: Logger
+
+    @RelaxedMockK
+    private lateinit var context: Context
+
     private lateinit var travellingAppImageLoader: TravellingAppImageLoader
 
     @Before
     fun setup() {
         MockKAnnotations.init(this)
-        travellingAppImageLoader = TravellingAppImageLoader(imageLoader)
+        travellingAppImageLoader = TravellingAppImageLoader(imageLoader, context, logger)
     }
 
     @Test
-    fun `should load image from network`() = runBlocking {
+    fun `should call third party image loader to load image from network`() = runBlocking {
         val imageUrl = "/someImageUrl"
-        val expectedImage = mockk<Drawable>()
+        val loadImageRequest = slot<ImageRequest>()
 
-        val loadImageRequest = mockk<ImageRequest>(relaxed = true){
-            every { data } returns imageUrl
+        coEvery { imageLoader.execute(capture(loadImageRequest)) } returns mockk(relaxed = true)
+
+        travellingAppImageLoader.loadFromNetwork(imageUrl)
+
+        coVerify {
+            imageLoader.execute(loadImageRequest.captured)
         }
-        val imageResult = mockk<ImageResult>(relaxed = true){
-            every { drawable } returns expectedImage
-        }
-
-        coEvery { imageLoader.execute(loadImageRequest) } returns imageResult
-
-        val resultImage = travellingAppImageLoader.loadFromNetwork(imageUrl)
-
-        assertEquals(expectedImage, resultImage)
     }
 
     @Test(expected = ImageLoadError::class)
-    fun `should fail to load images from network when image loader has errors`(): Unit = runBlocking {
-        val imageUrls = listOf("/someImageUrl", "/anotherImageUrl")
+    fun `should fail to load images from network when image loader has errors`(): Unit =
+        runBlocking {
+            val imageUrls = listOf("/someImageUrl", "/anotherImageUrl")
 
-        coEvery { imageLoader.execute(any()) } throws Exception()
+            coEvery { imageLoader.execute(any()) } throws Exception()
 
-        travellingAppImageLoader.loadFromNetwork(imageUrls)
-    }
+            travellingAppImageLoader.loadFromNetwork(imageUrls)
+        }
 }
