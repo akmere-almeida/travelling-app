c68d5263bc4a027971f73dadce16466a7fa135d6
Akme-re <akmere.almeida@thoughtworks.com>
2021-10-17 13:10:39 -0300

    refactor: add missing dependencies and organize code

--- app/src/main/java/com/akmere/travelling_app/dependencies/AppDependencies.kt
+++ app/src/main/java/com/akmere/travelling_app/dependencies/AppDependencies.kt
@@ -1,7 +1,10 @@
 package com.akmere.travelling_app.dependencies
 
 import android.content.Context
+import android.location.Geocoder
 import coil.ImageLoader
+import com.akmere.travelling_app.BuildConfig
+import com.akmere.travelling_app.common.AddressProvider
 import com.akmere.travelling_app.common.DebugLogger
 import com.akmere.travelling_app.common.Logger
 import com.akmere.travelling_app.data.model.PackageOfferData
@@ -10,6 +13,7 @@ import com.akmere.travelling_app.data.repository.OfferRepository
 import com.akmere.travelling_app.data.repository.PackageOfferRepository
 import com.akmere.travelling_app.domain.SearchOffers
 import com.akmere.travelling_app.domain.TravellingAppImageLoader
+import com.akmere.travelling_app.presentation.home.model.FilterOptions
 import com.apollographql.apollo.ApolloClient
 import okhttp3.OkHttpClient
 
@@ -19,6 +23,10 @@ object AppDependencies {
 
     fun providesAppImageLoader(context: Context): TravellingAppImageLoader =
         TravellingAppImageLoader(providesCoilImageLoader(context), context, logger)
+
+    fun providesAddressProvider(context: Context): AddressProvider {
+        return AddressProvider(providesGeocoder(context), logger)
+    }
 
     private val packageOfferRepository: OfferRepository<PackageOfferData> by lazy {
         PackageOfferRepository(apolloClient, logger)
@@ -32,11 +40,19 @@ object AppDependencies {
         OkHttpClient.Builder().build()
     }
 
-    private val logger: Logger by lazy {
-        DebugLogger()
+    private val logger: Logger? by lazy {
+        return@lazy if (BuildConfig.DEBUG) {
+            DebugLogger()
+        } else {
+            null
+        }
     }
 
     private fun providesCoilImageLoader(context: Context): ImageLoader {
         return ImageLoader.Builder(context).build()
+    }
+
+    private fun providesGeocoder(context: Context): Geocoder {
+        return Geocoder(context)
     }
 }
